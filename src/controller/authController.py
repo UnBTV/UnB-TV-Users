2562a3da389a826795e18e9622939e6f731fb612
Jauzimm <joaovitorso071@gmail.com>
2025-01-16 16:28:39 -0300

    Remoção do envio por admin

--- src/controller/authController.py
+++ src/controller/authController.py
@@ -2,22 +2,20 @@ import os
 import re 
 from typing import List
 from fastapi import APIRouter, HTTPException, Response, status, Depends
-from  utils import security, enumeration, send_mail
-from  database.database import get_db
+from utils import security, enumeration, send_mail
+from database.database import get_db
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-from  constants import errorMessages
+from constants import errorMessages
 from starlette.responses import JSONResponse
 from utils import security, enumeration
-
-from  domain import userSchema, authSchema
-from  repository import userRepository
+from domain import userSchema, authSchema
+from repository import userRepository
 
 auth = APIRouter(
   prefix="/auth"
 )
 
-  # Retorna conexões disponíveis 
 @auth.get("/vinculo", response_model=authSchema.Connections)
 def get_connection():
     connections = [member.value for member in enumeration.UserConnection]
@@ -25,122 +23,110 @@ def get_connection():
 
 @auth.post('/register')
 async def register(data: authSchema.UserCreate, db: Session = Depends(get_db)):
-# Verifica se connection é valido
-  if (not enumeration.UserConnection.has_value(data.connection)):
-    raise HTTPException(status_code=400, detail=errorMessages.INVALID_CONNECTION)
+    if not enumeration.UserConnection.has_value(data.connection):
+        raise HTTPException(status_code=400, detail=errorMessages.INVALID_CONNECTION)
 
-  # Validação de senha
-  is_valid = security.validate_password(data.password.strip()) 
-  if (not is_valid):
-    raise HTTPException(status_code=400, detail=errorMessages.INVALID_PASSWORD)
+    is_valid = security.validate_password(data.password.strip()) 
+    if not is_valid:
+        raise HTTPException(status_code=400, detail=errorMessages.INVALID_PASSWORD)
 
-  # Verifica se já existe um usuário com o email informado
-  user = userRepository.get_user_by_email(db, data.email)
-  if user:
-    raise HTTPException(status_code=400, detail=errorMessages.EMAIL_ALREADY_REGISTERED)
-  
-  hashed_password = security.get_password_hash(data.password)
-  
-  activation_code = security.generate_six_digit_number_code()
+    user = userRepository.get_user_by_email(db, data.email)
+    if user:
+        raise HTTPException(status_code=400, detail=errorMessages.EMAIL_ALREADY_REGISTERED)
 
-  userRepository.create_user(db, name=data.name, connection=data.connection, email=data.email, password=hashed_password, activation_code=activation_code)
-  
-  if re.search(r"unb", data.email):
-    await send_mail.send_verification_code(email=data.email, code=activation_code, is_unb=True)
-  else:
+    hashed_password = security.get_password_hash(data.password)
+    activation_code = security.generate_six_digit_number_code()
+
+    userRepository.create_user(
+        db, name=data.name, connection=data.connection, email=data.email, 
+        password=hashed_password, activation_code=activation_code
+    )
+
     await send_mail.send_verification_code(email=data.email, code=activation_code)
 
-  return JSONResponse(status_code=201, content={ "status": "success" })
+    return JSONResponse(status_code=201, content={"status": "success"})
 
-  # Recebe os dados de login
 @auth.post("/login", response_model=authSchema.Token)
 async def login(data: authSchema.UserLogin, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
-  
-  password_match = security.verify_password(data.password, user.password)
-  if not password_match:
-    raise HTTPException(status_code=404, detail=errorMessages.PASSWORD_NO_MATCH)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  if not user.is_active:
-    raise HTTPException(status_code=401, detail=errorMessages.ACCOUNT_IS_NOT_ACTIVE)
-  
-  access_token = security.create_access_token(data={ "id": user.id, "email": user.email, "role": user.role })
-  refresh_token = security.create_refresh_token(data={ "id": user.id })
+    password_match = security.verify_password(data.password, user.password)
+    if not password_match:
+        raise HTTPException(status_code=404, detail=errorMessages.PASSWORD_NO_MATCH)
 
-  return JSONResponse(status_code=200, content={ "access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer" })
+    if not user.is_active:
+        raise HTTPException(status_code=401, detail=errorMessages.ACCOUNT_IS_NOT_ACTIVE)
 
-  # Recebe os dados do usuário provenientes de uma autenticação social
+    access_token = security.create_access_token(data={"id": user.id, "email": user.email, "role": user.role})
+    refresh_token = security.create_refresh_token(data={"id": user.id})
+
+    return JSONResponse(status_code=200, content={"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"})
+
 @auth.post("/login/social")
 async def login_social(user: authSchema.UserSocial, db: Session = Depends(get_db)):
-  existing_user = userRepository.get_user_by_email(db, user.email)
-  
-  if existing_user is None:
-    new_user = userRepository.create_user_social(db, user.name, user.email)
-    user_id = new_user.id
-    is_new_user = True
-  else:
-    user_id = existing_user.id
-    is_new_user = False
+    existing_user = userRepository.get_user_by_email(db, user.email)
 
-  access_token = security.create_access_token(data={"id": user_id, "email": user.email, "role": "user"})
-  refresh_token = security.create_refresh_token(data={"id": user_id})
+    if existing_user is None:
+        new_user = userRepository.create_user_social(db, user.name, user.email)
+        user_id = new_user.id
+        is_new_user = True
+    else:
+        user_id = existing_user.id
+        is_new_user = False
 
-  return JSONResponse(status_code=200, content={
-    "access_token": access_token,
-    "refresh_token": refresh_token,
-    "token_type": "bearer",
-    "is_new_user": is_new_user,
-    "user_id": user_id
-  })
+    access_token = security.create_access_token(data={"id": user_id, "email": user.email, "role": "user"})
+    refresh_token = security.create_refresh_token(data={"id": user_id})
 
-  # trata da renovação de tokens de acesso      
+    return JSONResponse(status_code=200, content={
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+        "is_new_user": is_new_user,
+        "user_id": user_id
+    })
+
 @auth.post("/refresh", response_model=authSchema.RefreshTokenResponse)
 def refresh_token(token: dict = Depends(security.verify_token)):
-  access_token=security.create_access_token(token)
-  return JSONResponse(status_code=200, content={ "access_token": access_token, "token_type": "bearer" })
+    access_token = security.create_access_token(token)
+    return JSONResponse(status_code=200, content={"access_token": access_token, "token_type": "bearer"})
 
 @auth.post('/resend-code')
 async def send_new_code(data: authSchema.SendNewCode, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  if user.is_active:
-    return JSONResponse(status_code=400, content={ "status": "error", "message": errorMessages.ACCOUNT_ALREADY_ACTIVE })
+    if user.is_active:
+        return JSONResponse(status_code=400, content={"status": "error", "message": errorMessages.ACCOUNT_ALREADY_ACTIVE})
 
-  return JSONResponse(status_code=201, content={ "status": "success" })
+    return JSONResponse(status_code=201, content={"status": "success"})
 
-  # Recebe dados de validação de conta
 @auth.patch('/activate-account')
 async def validate_account(data: authSchema.AccountValidation, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  if user.is_active:
-    return JSONResponse(status_code=200, content={ "status": "error", "message": errorMessages.ACCOUNT_ALREADY_ACTIVE })
-  
-  if user.activation_code != data.code:
-    raise HTTPException(status_code=404, detail=errorMessages.INVALID_CODE)
+    if user.is_active:
+        return JSONResponse(status_code=200, content={"status": "error", "message": errorMessages.ACCOUNT_ALREADY_ACTIVE})
 
-  userRepository.activate_account(db, user)
-  return JSONResponse(status_code=200, content={ "status": "success" })
+    if user.activation_code != data.code:
+        raise HTTPException(status_code=404, detail=errorMessages.INVALID_CODE)
 
- # cadastro da senha de admin / role do admin
+    userRepository.activate_account(db, user)
+    return JSONResponse(status_code=200, content={"status": "success"})
+
 @auth.post('/admin-setup')
 async def admin_setup(data: authSchema.AdminSetup, db: Session = Depends(get_db), token: dict = Depends(security.verify_token_admin)):
     user = userRepository.get_user_by_email(db, data.email)
     if not user:
-      raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
     if not user.is_active:
-      raise HTTPException(status_code=400, detail="Account is not active")
+        raise HTTPException(status_code=400, detail="Account is not active")
 
-    if not re.search(r"unb", data.email):
-      raise HTTPException(status_code=400, detail="Account is not @unb")
-    
     userRepository.update_user_role(db, user, "COADMIN")
 
     return JSONResponse(status_code=200, content={"status": "success"})
@@ -149,68 +135,60 @@ async def admin_setup(data: authSchema.AdminSetup, db: Session = Depends(get_db)
 async def super_admin_setup(data: authSchema.AdminSetup, db: Session = Depends(get_db), token: dict = Depends(security.verify_token_admin)):
     user = userRepository.get_user_by_email(db, data.email)
     if not user:
-      raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
     if not user.is_active:
-      raise HTTPException(status_code=400, detail="Account is not active")
+        raise HTTPException(status_code=400, detail="Account is not active")
 
-    if not re.search(r"unb", data.email):
-      raise HTTPException(status_code=400, detail="Account is not @unb")
-    
     userRepository.update_user_role(db, user, "ADMIN")
 
     return JSONResponse(status_code=200, content={"status": "success"})
 
 @auth.post('/reset-password/request')
 async def request_password_(data: authSchema.ResetPasswordRequest, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  if not user.is_active:
-    raise HTTPException(status_code=404, detail=errorMessages.ACCOUNT_IS_NOT_ACTIVE)
-  
-  code = security.generate_six_digit_number_code()
+    if not user.is_active:
+        raise HTTPException(status_code=404, detail=errorMessages.ACCOUNT_IS_NOT_ACTIVE)
 
-  userRepository.set_user_reset_pass_code(db, user, code)
-  await send_mail.send_reset_password_code(data.email, code)
-  return JSONResponse(status_code=200, content={ "status": "success" })
+    code = security.generate_six_digit_number_code()
+
+    userRepository.set_user_reset_pass_code(db, user, code)
+    await send_mail.send_reset_password_code(data.email, code)
+    return JSONResponse(status_code=200, content={"status": "success"})
 
 @auth.post('/reset-password/verify')
 async def verify_reset_code(data: authSchema.ResetPasswordVerify, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
-  
-  if not user.password_reset_code:
-    raise HTTPException(status_code=404, detail=errorMessages.NO_RESET_PASSWORD_CODE)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  if user.password_reset_code != data.code:
-    raise HTTPException(status_code=400, detail=errorMessages.INVALID_RESET_PASSWORD_CODE)
+    if not user.password_reset_code:
+        raise HTTPException(status_code=404, detail=errorMessages.NO_RESET_PASSWORD_CODE)
 
-  return JSONResponse(status_code=200, content={ "status": "success" })
+    if user.password_reset_code != data.code:
+        raise HTTPException(status_code=400, detail=errorMessages.INVALID_RESET_PASSWORD_CODE)
 
-  # Atualizar senha de um usuário após uma solicitação de redefinição
+    return JSONResponse(status_code=200, content={"status": "success"})
+
 @auth.patch('/reset-password/change', response_model=userSchema.User)
 async def update_user_password(data: authSchema.ResetPasswordUpdate, db: Session = Depends(get_db)):
-  user = userRepository.get_user_by_email(db, data.email)
-  if not user:
-    raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
-  
-  # Valida a senha informada
-  if data.password and not security.validate_password(data.password):
-    raise HTTPException(status_code=400, detail=errorMessages.INVALID_PASSWORD)
+    user = userRepository.get_user_by_email(db, data.email)
+    if not user:
+        raise HTTPException(status_code=404, detail=errorMessages.USER_NOT_FOUND)
 
-  # Verifica se o usuario possui um reset code. Se não possuir, a solicitação é invalida e deve ser bloqueada
-  if not user.password_reset_code:
-    raise HTTPException(status_code=401, detail=errorMessages.INVALID_REQUEST)
-  
-  # Verifica se o código corresponde
-  if data.code != user.password_reset_code:
-    raise HTTPException(status_code=400, detail=errorMessages.INVALID_RESET_PASSWORD_CODE)
-    
-  # Faz procedimento de hash da senha e atualiza usuario
-  hashed_password = security.get_password_hash(data.password)
-  updated_user = userRepository.update_password(db, user, hashed_password)
+    if data.password and not security.validate_password(data.password):
+        raise HTTPException(status_code=400, detail=errorMessages.INVALID_PASSWORD)
 
-  return updated_user
+    if not user.password_reset_code:
+        raise HTTPException(status_code=401, detail=errorMessages.INVALID_REQUEST)
+
+    if data.code != user.password_reset_code:
+        raise HTTPException(status_code=400, detail=errorMessages.INVALID_RESET_PASSWORD_CODE)
+
+    hashed_password = security.get_password_hash(data.password)
+    updated_user = userRepository.update_password(db, user, hashed_password)
+
+    return updated_user
--- src/utils/send_mail.py
+++ src/utils/send_mail.py
@@ -5,52 +5,48 @@ from dotenv import load_dotenv
 from pydantic import BaseModel, EmailStr
 from starlette.responses import JSONResponse
 from typing import List
-from  domain import authSchema
+from domain import authSchema
 
 conf = ConnectionConfig(
-  MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
-  MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
-  MAIL_FROM = os.getenv("MAIL_FROM"),
-  MAIL_PORT = os.getenv("MAIL_PORT"),
-  MAIL_SERVER = os.getenv("MAIL_SERVER"),
-  MAIL_FROM_NAME="UNB TV",
-  MAIL_STARTTLS = True,
-  MAIL_SSL_TLS = False,
-  USE_CREDENTIALS = True,
-  VALIDATE_CERTS = True
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME="UNB TV",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
 
 fm = FastMail(conf)
 
-async def send_verification_code(email: str, code: int, is_unb: bool =False) -> JSONResponse:
-  html = f"<p>Seja bem-vindo ao UnB-TV! Para confirmar a criação da sua conta, utilize o código <strong>{code}</strong></p>"
-  
-  DEPLOY_URL = os.getenv("DEPLOY_URL")
-  if is_unb:
-      html += f"<p>Como usuário da UnB, você pode configurar uma senha de administrador acessando o seguinte link após ativar sua conta: <a href='{DEPLOY_URL}/adminActivate?email={email}'>Configurar Senha de Administrador</a></p>"
+async def send_verification_code(email: str, code: int) -> JSONResponse:
+    html = f"<p>Seja bem-vindo ao UnB-TV! Para confirmar a criação da sua conta, utilize o código <strong>{code}</strong></p>"
 
-  message = MessageSchema(
-    subject="Confirme a criação da sua conta",
-    recipients=[email],
-    body=html,
-    subtype=MessageType.html
-  )
-  
-  await fm.send_message(message)
-  return JSONResponse(status_code=200, content={ "status": "success" })
+    message = MessageSchema(
+        subject="Confirme a criação da sua conta",
+        recipients=[email],
+        body=html,
+        subtype=MessageType.html
+    )
 
-async def send_reset_password_code(email: str, code: int) -> JSONResponse:  
-  html = f"""
-    <p>Foi feita uma solicitação de troca de senha. Caso você tenha feito essa solicitação, utilize o código <strong>{code}</strong para trocar a sua senha.</p>
-    <p>Caso você não tenha feito essa solicitação, por favor ignore este email</p>
-  """
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"status": "success"})
 
-  message = MessageSchema(
-    subject="Confirme a troca da sua senha",
-    recipients=[email],
-    body=html,
-    subtype=MessageType.html
-  )
-  
-  await fm.send_message(message)
-  return JSONResponse(status_code=200, content={ "status": "success" })
+async def send_reset_password_code(email: str, code: int) -> JSONResponse:
+    html = f"""
+        <p>Foi feita uma solicitação de troca de senha. Caso você tenha feito essa solicitação, utilize o código <strong>{code}</strong> para trocar a sua senha.</p>
+        <p>Caso você não tenha feito essa solicitação, por favor ignore este email</p>
+    """
+
+    message = MessageSchema(
+        subject="Confirme a troca da sua senha",
+        recipients=[email],
+        body=html,
+        subtype=MessageType.html
+    )
+
+    await fm.send_message(message)
+    return JSONResponse(status_code=200, content={"status": "success"})
